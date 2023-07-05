510281676a8035cb58cc9642f0630e92e2e0063b
Alejo Boga <alejoboga20@gmail.com>
2023-07-05 15:59:21 -0400

    find habitable planets

--- planets-project/index.ts
+++ planets-project/index.ts
@@ -1,14 +1,20 @@
 import fs from 'fs';
 import { parse } from 'csv-parse';
+import { Planet } from './types/Planets';
 
-const results: (string | Buffer)[] = [];
+const results: Planet[] = [];
+
+const isHabitablePlanet = ({ koi_disposition, koi_insol, koi_prad }: Planet) =>
+	koi_disposition === 'CONFIRMED' && +koi_insol > 0.36 && +koi_insol < 1.11 && +koi_prad < 1.6;
 
 //Read the CSV file and save it in results
 const readStreamEvent = fs.createReadStream('./data/kepler_data.csv');
 
 //Use a pipe to parse the CSV file as js objects
-readStreamEvent.pipe(parse({ comment: '#', columns: true })).on('data', (data) => {
-	results.push(data);
+readStreamEvent.pipe(parse({ comment: '#', columns: true })).on('data', (data: Planet) => {
+	if (isHabitablePlanet(data)) {
+		results.push(data);
+	}
 });
 
 readStreamEvent.on('error', (err) => {
