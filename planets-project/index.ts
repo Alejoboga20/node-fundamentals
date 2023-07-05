e11ee2f49fea6c091f7b868cf5c8339f24b4587b
Alejo Boga <alejoboga20@gmail.com>
2023-07-05 15:17:45 -0400

    read file

--- planets-project/index.ts
+++ planets-project/index.ts
@@ -1,1 +1,17 @@
+import fs from 'fs';
 import { parse } from 'csv-parse';
+
+const results: (string | Buffer)[] = [];
+
+//Read the CSV file and save it in results
+fs.createReadStream('./data/kepler_data.csv')
+	.on('data', (data) => {
+		results.push(data);
+	})
+	.on('error', (err) => {
+		console.log(err);
+	})
+	.on('end', () => {
+		console.log('finish reading file');
+		console.log({ results });
+	});
