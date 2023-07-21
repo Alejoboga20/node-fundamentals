c011f307b82084d7fd74fd8758f41b0a157e275a
Alejo Boga <alejoboga20@gmail.com>
2023-07-21 14:05:25 -0400

    hello world

--- express-intro/src/index.ts
+++ express-intro/src/index.ts
@@ -3,4 +3,9 @@ import express from 'express';
 const PORT = 3000;
 
 const app = express();
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello World!</h1>');
+});
+
 app.listen(PORT, () => console.log(`express app listening on port ${PORT}!`));
