07e6489af8cdae3c4de75a605cf437227eeedbb1
Alejo Boga <alejoboga20@gmail.com>
2023-07-21 14:12:18 -0400

    route paramenters

--- express-intro/src/index.ts
+++ express-intro/src/index.ts
@@ -2,10 +2,39 @@ import express from 'express';
 
 const PORT = 3000;
 
+enum StatusCode {
+	OK = 200,
+	NOT_FOUND = 404,
+}
+
+const friends = [
+	{
+		id: 0,
+		name: 'Ross Geller',
+	},
+	{
+		id: 1,
+		name: 'Chandler Bing',
+	},
+	{
+		id: 2,
+		name: 'Joey Tribbiani',
+	},
+];
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('<h1>Hello World!</h1>');
+app.get('/friends', (req, res) => {
+	res.json({ friends });
+});
+
+app.get('/friends/:friendId', (req, res) => {
+	const friendId = Number(req.params.friendId);
+	const friend = friends[friendId];
+
+	if (friend) res.status(StatusCode.OK).json({ friend });
+
+	res.status(StatusCode.NOT_FOUND).json({ error: 'Friend not found' });
 });
 
 app.listen(PORT, () => console.log(`express app listening on port ${PORT}!`));
