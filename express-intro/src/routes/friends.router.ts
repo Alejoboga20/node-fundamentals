ad9bd1c90f17e2966d7bad73419b633f9bd76168
Alejo Boga <alejoboga20@gmail.com>
2023-07-24 14:56:16 -0400

    use middleware only in friends router

--- express-intro/src/routes/friends.router.ts
+++ express-intro/src/routes/friends.router.ts
@@ -4,6 +4,11 @@ import FriendsController from '../controllers/friends.controller';
 
 const friendsRouter = express.Router();
 
+friendsRouter.use((req, _, next) => {
+	console.log('IP ADDRESS: ', req.ip);
+	next();
+});
+
 friendsRouter.get('/', FriendsController.getFriends);
 friendsRouter.post('/', FriendsController.createFriend);
 friendsRouter.get('//:friendId', FriendsController.getFriendById);
