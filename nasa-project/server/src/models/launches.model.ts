d43e38a2260bd5f32fd468cb2a8f8a5e400252be
Alejo Boga <alejoboga20@gmail.com>
2023-08-16 15:28:44 -0300

    add launche model

--- nasa-project/server/src/models/launches.model.ts
+++ nasa-project/server/src/models/launches.model.ts
@@ -1,6 +1,19 @@
+type Launch = {
+	flightNumber: number;
+	mission: string;
+	rocket: string;
+	destination: string;
+	customers: string[];
+	launchDate: Date;
+	upcoming: boolean;
+	success: boolean;
+};
+
 const launches = new Map();
 
-const launch = {
+let latestFlightNumber = 100;
+
+const launch: Launch = {
 	flightNumber: 100,
 	mission: 'Kepler Exploration X',
 	rocket: 'Explorer IS1',
@@ -14,3 +27,16 @@ const launch = {
 launches.set(launch.flightNumber, launch);
 
 export const getAllLaunches = () => Array.from(launches.values());
+
+export const addNewLaunch = (launch: Launch) => {
+	latestFlightNumber++;
+	launches.set(
+		latestFlightNumber,
+		Object.assign(launch, {
+			flightNumber: latestFlightNumber,
+			customers: ['NASA', 'ZTM'],
+			upcoming: true,
+			success: true,
+		})
+	);
+};
