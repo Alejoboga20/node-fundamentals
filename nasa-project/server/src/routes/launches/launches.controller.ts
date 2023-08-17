ac61502ba72a7dccacb118c722285a8cddc09467
Alejo Boga <alejoboga20@gmail.com>
2023-08-17 14:23:10 -0300

    add validations

--- nasa-project/server/src/routes/launches/launches.controller.ts
+++ nasa-project/server/src/routes/launches/launches.controller.ts
@@ -9,7 +9,13 @@ export const httpGetAllLaunches = async (_: Request, res: Response) => {
 
 export const httpAddNewLaunch = async (req: Request, res: Response) => {
 	const data: Launch = req.body;
+
+	if (!data.mission || !data.rocket || !data.launchDate || !data.destination)
+		return res.status(400).json({ error: 'Bad Request - Missing Params' });
+
 	data.launchDate = new Date(data.launchDate);
+	if (data.launchDate.toString() === 'Invalid Date')
+		return res.status(400).json({ error: 'Bad Request - Invalid Date' });
 
 	addNewLaunch(data);
 	return res.status(201).json({ message: 'Launch added', launch: data });
