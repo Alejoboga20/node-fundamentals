7ac77a88add5d42619ac6bbaab5dfee8305b4861
Alejo Boga <alejoboga20@gmail.com>
2023-08-17 14:47:19 -0300

    send launch post

--- nasa-project/client/src/pages/Launch.js
+++ nasa-project/client/src/pages/Launch.js
@@ -4,7 +4,7 @@ import Clickable from '../components/Clickable';
 
 const Launch = ({ entered, planets, submitLaunch, isPendingLaunch }) => {
 	const selectorBody = useMemo(() => {
-		return planets.planets?.map((planet) => (
+		return planets.map((planet) => (
 			<option value={planet.kepler_name} key={planet.kepler_name}>
 				{planet.kepler_name}
 			</option>
