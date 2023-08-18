2f22fd1bc6ce4cd6472f81366192429e5849f430
Alejo Boga <alejoboga20@gmail.com>
2023-08-18 10:59:23 -0300

    use supertest

--- nasa-project/server/src/routes/launches/launches.test.ts
+++ nasa-project/server/src/routes/launches/launches.test.ts
@@ -1,12 +1,16 @@
+import request from 'supertest';
+import app from '../../app';
+
 enum StatusCode {
 	OK = 200,
 	BAD_REQUEST = 400,
 }
 
+const server = request(app);
+
 describe('Test GET /launches', () => {
-	test('It should respond with 200 success', () => {
-		const response = 200;
-		expect(response).toBe(StatusCode.OK);
+	test('It should respond with 200 success', async () => {
+		await server.get('/api/v1/launches').expect(StatusCode.OK).expect('Content-Type', /json/);
 	});
 });
 
